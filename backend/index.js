6b7a1deb134bacd066d4ea18ed8cb56e09500c2e
VSB14 <vijaysinghbhandari419@gmail.com>
2024-11-08 17:30:09 +0530

    Removed the trailing slash from CORS origin and baseURL in apiconnector

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://ed-up.vercel.app/",
+		origin:"https://ed-up.vercel.app",
 		// origin:'*',
 		credentials:true,
 	})
